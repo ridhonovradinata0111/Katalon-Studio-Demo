8f7b7ead5d50c480bda18c409dc0c172c1074272
Ridho Novradinata <ridho.novradinata@bni.co.id>
2023-11-02 16:55:51 +0700

    Add Test Suite

---
+++ Test Suites/BookAppointment.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BookAppointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1a0a16bb-1e47-4f1b-abae-cceddd9a56b1</testSuiteGuid>
+</TestSuiteEntity>
